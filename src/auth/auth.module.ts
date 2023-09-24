c85178337952593b336e7798011bc37a35e913a6
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2023-09-24 22:22:41 +0900

    :tada: Auth gateway completed

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Global, Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AUTH_PACKAGE_NAME, AUTH_SERVICE_NAME } from './auth.pb';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+
+@Global()
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: AUTH_SERVICE_NAME,
+        transport: Transport.GRPC,
+        options: {
+          url: '0.0.0.0:50051',
+          package: AUTH_PACKAGE_NAME,
+          protoPath: 'node_modules/grpc-nest-proto/proto/auth.proto',
+        },
+      },
+    ]),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
+})
+export class AuthModule { }
